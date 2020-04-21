2666899f4327062330997fbe4aebd59284b19452
賬號已註銷 <wbyanyue@centaline.com.cn>
2020-04-21 18:04:58 +0800

    webpack配置修改

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,8 @@ const common=require('./webpack.common')
 
 module.exports=merge(common,{
     mode:'development',
-    devtool:'source-map',
-        devServer:{
+    devtool:'inline-source-map',
+    devServer:{
         contentBase:'./dist',
         compress:true,
         // 模块热替换和splitChunks不能同时使用
